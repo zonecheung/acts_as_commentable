046cddd6808f9f5f50cf5b8f7d5176f1d4b5cce7
John Tjanaka <zonecheung@gmail.com>
2020-07-28 17:09:55 +0700

    Use reverse_merge to prevent the Ruby 2.7.1 deprecation warnings.

--- lib/commentable_methods.rb
+++ lib/commentable_methods.rb
@@ -53,9 +53,9 @@ module Juixe
             comment_roles.each do |role|
               define_role_based_inflection(role)
             end
-            has_many :all_comments, { :as => :commentable, :dependent => :destroy, class_name: 'Comment' }.merge(join_options)
+	    has_many :all_comments, join_options.reverse_merge(as: :commentable, dependent: :destroy, class_name: 'Comment')
           else
-            has_many :comments, {:as => :commentable, :dependent => :destroy}.merge(join_options)
+		  has_many :comments, join_options.reverse_merge(as: :commentable, dependent: :destroy)
           end
 
           comment_types.each do |role|
